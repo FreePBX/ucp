1da86e7ef408a3290222c0cd5b1df02f796be899
Andrew Nagy <andrew.nagy@the159.com>
2014-01-31 22:31:09 -0500

    Added license information

--- htdocs/includes/UCP.class.php
+++ htdocs/includes/UCP.class.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * This is the User Control Panel Object.
+ *
+ * Copyright (C) 2013 Schmooze Com, INC
+ * Copyright (C) 2013 Andrew Nagy <andrew.nagy@schmoozecom.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package   FreePBX UCP BMO
+ * @author   Andrew Nagy <andrew.nagy@schmoozecom.com>
+ * @license   AGPL v3
+ */
 namespace UCP;
 include(__DIR__.'/UCP_Helpers.class.php');
 class UCP extends UCP_Helpers {
@@ -26,12 +49,12 @@ class UCP extends UCP_Helpers {
 	/**
 	 * Alternative Constructor
 	 *
-	 * This allows the Current BMO to be referenced from anywhere, without
-	 * needing to instantiate a new one. Calling $x = FreePBX::create() will
-	 * create a new BMO if one has not already beeen created (unlikely!), or
+	 * This allows the Current UCP to be referenced from anywhere, without
+	 * needing to instantiate a new one. Calling $x = UCP::create() will
+	 * create a new UCP if one has not already beeen created (unlikely!), or
 	 * return a reference to the current one.
 	 *
-	 * @return object FreePBX BMO Object
+	 * @return object FreePBX UCP Object
 	 */
 	public static function create() {
 		if (!isset(self::$obj)) {
