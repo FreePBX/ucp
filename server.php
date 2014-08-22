af14490709d9acbdbbab24e85f1af4b2b39c256c
Andrew Nagy <andrew.nagy@the159.com>
2014-08-21 22:50:11 -0400

    forgot the server file for websockets

--- server.php
+++ server.php
@@ -12,22 +12,47 @@ $bootstrap_settings['freepbx_auth'] = false;
 //TODO: We need to make sure security is 100%!
 $restrict_mods = true; //Set to true so that we just load framework and the page wont bomb out because we have no session
 if (!@include_once(getenv('FREEPBX_CONF') ? getenv('FREEPBX_CONF') : '/etc/freepbx.conf')) {
-      include_once('/etc/asterisk/freepbx.conf');
+    include_once('/etc/asterisk/freepbx.conf');
 }
 include(dirname(__FILE__).'/htdocs/includes/UCP.class.php');
 $ucp = \UCP\UCP::create();
 ob_end_clean();
 
+fwrite(STDOUT, "Starting \n");
+
+stream_set_timeout($astman->socket, 1);
+stream_set_blocking($astman->socket, 0);
+
+$astman->Events("on");
+
+$astman->add_event_handler("hangup", "output");
+$lastping = null;
+$stdin = fopen('php://stdin', 'r');
+
 while(true) {
-	$stdin = fopen('php://stdin', 'r');
-	$line = fgets($stdin);
-	stream_set_blocking($stdin,0);
-	$message = array('message' => 'user');
-	$line = fgets($stdin);
-	if($line !== false) {
-		$message['response'] = 'Hello ' . trim($line) . "!\n";
-	}
-	fwrite(STDOUT, json_encode($message));
-	fwrite(STDOUT, "\n");
-	sleep(1);
+  $astman->wait_response(true);
+  usleep(500000);
+  if(empty($lastping)) {
+    fwrite(STDOUT, json_encode(array("event"=>"ping"))."\n");
+    stream_set_blocking($stdin, 1);
+    $line = fgets($stdin);
+    stream_set_blocking($stdin, 0);
+    if($line !== false) {
+      $json = json_decode($line,true);
+      if($json['event'] == "pong") {
+        fwrite(STDOUT, json_encode(array("event"=>"gotcha"))."\n");
+        $lastping = time();
+      } else {
+        return false;
+      }
+    }
+  }
+}
+
+function output($e, $p, $server, $port) {
+  $array = array(
+    "event" => $e,
+    "data" => $p
+  );
+  fwrite(STDOUT, json_encode($array)."\n");
 }
