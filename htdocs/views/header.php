368d374dd6301e40afb7a0a6def4b008e803d443
Andrew Nagy <andrew.nagy@the159.com>
2015-01-26 20:56:55 -0500

    FREEPBX-8321 touch icon for apple/android devices

--- htdocs/views/header.php
+++ htdocs/views/header.php
@@ -19,9 +19,29 @@
 		<link href="assets/css/compiled/modules/<?php echo $ucpmoduleless?>" rel="stylesheet" type="text/css">
 
 		<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1">
+		<!-- Apple Specific -->
 		<meta name="apple-mobile-web-app-capable" content="yes" />
-		<link rel="apple-touch-icon" href="assets/images/badge.png" />
-		<link rel="apple-touch-icon-precomposed" href="assets/images/badge.png" />
+
+		<link rel="apple-touch-icon" href="assets/icons/60x60.png"> <!-- 60 x 60 -->
+		<link rel="apple-touch-icon" sizes="76x76" href="assets/icons/76x76.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="assets/icons/120x120.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="assets/icons/152x152.png">
+
+		<link rel="apple-touch-startup-image" href="assets/icons/320x480.png">
+
+		<meta name="apple-mobile-web-app-capable" content="yes">
+
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<!-- End Apple Specific -->
+
+		<link href="assets/icons/192x192.png" rel="icon" sizes="192x192" />
+		<link href="assets/icons/128x128.png" rel="icon" sizes="128x128" />
+		<!-- apple does not use this but android does -->
+		<link rel="apple-touch-icon-precomposed" sizes="128x128" href="assets/icons/128x128.png">
+		<link rel="apple-touch-icon" sizes="128x128" href="assets/icons/128x128.png">
+
+		<meta name="mobile-web-app-capable" content="yes">
+
 		<!--[if gte IE 9]>
 		  <style type="text/css">
 		    .gradient {
