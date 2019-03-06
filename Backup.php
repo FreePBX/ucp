bdf1ca8805a0bb36eb8341fdf97a87e518f78bee
Andrew Nagy <andrew.nagy@the159.com>
2019-03-06 18:10:40 -0500

    [Module Tag script: ucp 15.0.6]

--- Backup.php
+++ Backup.php
@@ -3,11 +3,13 @@ namespace FreePBX\modules\Ucp;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
 	public function runBackup($id,$transaction){
-		$configs = $this->dumpTables();
 		$this->addDependency('userman');
 		$this->addDependency('pm2');
 		$this->addDependency('certman');
 		$this->addDependency('core');
-		$this->addConfigs($configs);
+		$this->addConfigs([
+			'tables' => $this->dumpTables(),
+			'settings' => $this->dumpAdvancedSettings()
+		]);
 	}
 }
--- Restore.php
+++ Restore.php
@@ -2,12 +2,14 @@
 namespace FreePBX\modules\Ucp;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = $this->getConfigs();
-		$this->importTables($configs);
-  }
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		$this->importTables($configs['tables']);
+		$this->importAdvancedSettings($configs['settings']);
+	}
 
-  public function processLegacy($pdo, $data, $tables, $unknownTables){
+	public function processLegacy($pdo, $data, $tables, $unknownTables){
 		$this->restoreLegacyDatabase($pdo);
+		$this->restoreLegacyAdvancedSettings($pdo);
 	}
 }
