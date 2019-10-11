aa75503f192995e4cee4c6ce8e14de030b5749d2
Philip Joseph <jphilip@sangoma.com>
2019-10-11 05:32:24 +0000

    FREEPBX-20211 Composer Update JShrink library

--- htdocs/vendor/composer/autoload_real.php
+++ htdocs/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit15b5352b00b3e50849592447f8f8987f
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit15b5352b00b3e50849592447f8f8987f', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
