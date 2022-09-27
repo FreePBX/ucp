d1cef177dbc75bff1fba49e897ec82b09234cd19
Megha BM <meghabm@sangoma.com>
2022-09-27 07:41:52 +0000

    New Bootstrap GUI fixes

--- htdocs/assets/js/bootstrap-table-locale/bootstrap-table-de-DE.js
+++ htdocs/assets/js/bootstrap-table-locale/bootstrap-table-de-DE.js
@@ -2,36 +2,141 @@
 * Bootstrap Table German translation
 * Author: Paul Mohr - Sopamo<p.mohr@sopamo.de>
 */
-(function ($) {
-  'use strict';
 
-  $.fn.bootstrapTable.locales['de-DE'] = {
-    formatLoadingMessage: function () {
-      return 'Lade, bitte warten...';
-    },
-    formatRecordsPerPage: function (pageNumber) {
-      return pageNumber + ' Einträge pro Seite';
-    },
-    formatShowingRows: function (pageFrom, pageTo, totalRows) {
-      return 'Zeige ' + pageFrom + ' bis ' + pageTo + ' von ' + totalRows + ' Zeile' + ((totalRows > 1) ? "n" : "");
-    },
-    formatSearch: function () {
-      return 'Suchen';
-    },
-    formatNoMatches: function () {
-      return 'Keine passenden Ergebnisse gefunden';
-    },
-    formatRefresh: function () {
-      return 'Neu laden';
-    },
-    formatToggle: function () {
-      return 'Umschalten';
-    },
-    formatColumns: function () {
-      return 'Spalten';
+$.fn.bootstrapTable.locales['de-DE'] = $.fn.bootstrapTable.locales['de'] = {
+  formatCopyRows () {
+    return 'Zeilen kopieren'
+  },
+  formatPrint () {
+    return 'Drucken'
+  },
+  formatLoadingMessage () {
+    return 'Lade, bitte warten'
+  },
+  formatRecordsPerPage (pageNumber) {
+    return `${pageNumber} Zeilen pro Seite.`
+  },
+  formatShowingRows (pageFrom, pageTo, totalRows, totalNotFiltered) {
+    if (totalNotFiltered !== undefined && totalNotFiltered > 0 && totalNotFiltered > totalRows) {
+      return `Zeige Zeile ${pageFrom} bis ${pageTo} von ${totalRows} Zeile${(totalRows > 1) ? 'n' : ''} (Gefiltert von ${totalNotFiltered} Zeile${(totalNotFiltered > 1) ? 'n' : ''})`
     }
-  };
 
-    $.extend($.fn.bootstrapTable.defaults, $.fn.bootstrapTable.locales['de-DE']);
+    return `Zeige Zeile ${pageFrom} bis ${pageTo} von ${totalRows} Zeile${(totalRows > 1) ? 'n' : ''}.`
+  },
+  formatSRPaginationPreText () {
+    return 'Vorherige Seite'
+  },
+  formatSRPaginationPageText (page) {
+    return `Zu Seite ${page}`
+  },
+  formatSRPaginationNextText () {
+    return 'Nächste Seite'
+  },
+  formatDetailPagination (totalRows) {
+    return `Zeige ${totalRows} Zeile${(totalRows > 1) ? 'n' : ''}.`
+  },
+  formatClearSearch () {
+    return 'Lösche Filter'
+  },
+  formatSearch () {
+    return 'Suchen'
+  },
+  formatNoMatches () {
+    return 'Keine passenden Ergebnisse gefunden'
+  },
+  formatPaginationSwitch () {
+    return 'Verstecke/Zeige Nummerierung'
+  },
+  formatPaginationSwitchDown () {
+    return 'Zeige Nummerierung'
+  },
+  formatPaginationSwitchUp () {
+    return 'Verstecke Nummerierung'
+  },
+  formatRefresh () {
+    return 'Neu laden'
+  },
+  formatToggleOn () {
+    return 'Normale Ansicht'
+  },
+  formatToggleOff () {
+    return 'Kartenansicht'
+  },
+  formatColumns () {
+    return 'Spalten'
+  },
+  formatColumnsToggleAll () {
+    return 'Alle umschalten'
+  },
+  formatFullscreen () {
+    return 'Vollbild'
+  },
+  formatAllRows () {
+    return 'Alle'
+  },
+  formatAutoRefresh () {
+    return 'Automatisches Neuladen'
+  },
+  formatExport () {
+    return 'Datenexport'
+  },
+  formatJumpTo () {
+    return 'Springen'
+  },
+  formatAdvancedSearch () {
+    return 'Erweiterte Suche'
+  },
+  formatAdvancedCloseButton () {
+    return 'Schließen'
+  },
+  formatFilterControlSwitch () {
+    return 'Verstecke/Zeige Filter'
+  },
+  formatFilterControlSwitchHide () {
+    return 'Verstecke Filter'
+  },
+  formatFilterControlSwitchShow () {
+    return 'Zeige Filter'
+  },
+  formatAddLevel () {
+    return 'Ebene hinzufügen'
+  },
+  formatCancel () {
+    return 'Abbrechen'
+  },
+  formatColumn () {
+    return 'Spalte'
+  },
+  formatDeleteLevel () {
+    return 'Ebene entfernen'
+  },
+  formatDuplicateAlertTitle () {
+    return 'Doppelte Einträge gefunden!'
+  },
+  formatDuplicateAlertDescription () {
+    return 'Bitte doppelte Spalten entfenen oder ändern'
+  },
+  formatMultipleSort () {
+    return 'Mehrfachsortierung'
+  },
+  formatOrder () {
+    return 'Reihenfolge'
+  },
+  formatSort () {
+    return 'Sortieren'
+  },
+  formatSortBy () {
+    return 'Sortieren nach'
+  },
+  formatThenBy () {
+    return 'anschließend'
+  },
+  formatSortOrders () {
+    return {
+      asc: 'Aufsteigend',
+      desc: 'Absteigend'
+    }
+  }
+}
 
-})(jQuery);
+$.extend($.fn.bootstrapTable.defaults, $.fn.bootstrapTable.locales['de-DE'])
--- htdocs/assets/js/bootstrap-table-locale/bootstrap-table-es-AR.js
+++ htdocs/assets/js/bootstrap-table-locale/bootstrap-table-es-AR.js
@@ -1,31 +1,104 @@
 /**
  * Bootstrap Table Spanish (Argentina) translation
  * Author: Felix Vera (felix.vera@gmail.com)
+ * Edited by: DarkThinking (https://github.com/DarkThinking)
  */
-(function ($) {
-    'use strict';
 
-    $.fn.bootstrapTable.locales['es-AR'] = {
-        formatLoadingMessage: function () {
-            return 'Cargando, espere por favor...';
-        },
-        formatRecordsPerPage: function (pageNumber) {
-            return pageNumber + ' registros por página';
-        },
-        formatShowingRows: function (pageFrom, pageTo, totalRows) {
-            return 'Mostrando ' + pageFrom + ' a ' + pageTo + ' de ' + totalRows + ' filas';
-        },
-        formatSearch: function () {
-            return 'Buscar';
-        },
-        formatNoMatches: function () {
-            return 'No se encontraron registros';
-        },
-        formatAllRows: function () {
-            return 'Todo';
-        }
-    };
+$.fn.bootstrapTable.locales['es-AR'] = {
+  formatCopyRows () {
+    return 'Copiar Filas'
+  },
+  formatPrint () {
+    return 'Imprimir'
+  },
+  formatLoadingMessage () {
+    return 'Cargando, espere por favor'
+  },
+  formatRecordsPerPage (pageNumber) {
+    return `${pageNumber} registros por página`
+  },
+  formatShowingRows (pageFrom, pageTo, totalRows, totalNotFiltered) {
+    if (totalNotFiltered !== undefined && totalNotFiltered > 0 && totalNotFiltered > totalRows) {
+      return `Mostrando desde ${pageFrom} a ${pageTo} de ${totalRows} filas (filtrado de ${totalNotFiltered} columnas totales)`
+    }
 
-    $.extend($.fn.bootstrapTable.defaults, $.fn.bootstrapTable.locales['es-AR']);
+    return `Mostrando desde ${pageFrom} a ${pageTo} de ${totalRows} filas`
+  },
+  formatSRPaginationPreText () {
+    return 'página anterior'
+  },
+  formatSRPaginationPageText (page) {
+    return `a la página ${page}`
+  },
+  formatSRPaginationNextText () {
+    return 'siguiente página'
+  },
+  formatDetailPagination (totalRows) {
+    return `Mostrando ${totalRows} columnas`
+  },
+  formatClearSearch () {
+    return 'Limpiar búsqueda'
+  },
+  formatSearch () {
+    return 'Buscar'
+  },
+  formatNoMatches () {
+    return 'No se encontraron registros'
+  },
+  formatPaginationSwitch () {
+    return 'Ocultar/Mostrar paginación'
+  },
+  formatPaginationSwitchDown () {
+    return 'Mostrar paginación'
+  },
+  formatPaginationSwitchUp () {
+    return 'Ocultar paginación'
+  },
+  formatRefresh () {
+    return 'Recargar'
+  },
+  formatToggleOn () {
+    return 'Mostrar vista de carta'
+  },
+  formatToggleOff () {
+    return 'Ocultar vista de carta'
+  },
+  formatColumns () {
+    return 'Columnas'
+  },
+  formatColumnsToggleAll () {
+    return 'Cambiar todo'
+  },
+  formatFullscreen () {
+    return 'Pantalla completa'
+  },
+  formatAllRows () {
+    return 'Todo'
+  },
+  formatAutoRefresh () {
+    return 'Auto Recargar'
+  },
+  formatExport () {
+    return 'Exportar datos'
+  },
+  formatJumpTo () {
+    return 'Ir'
+  },
+  formatAdvancedSearch () {
+    return 'Búsqueda avanzada'
+  },
+  formatAdvancedCloseButton () {
+    return 'Cerrar'
+  },
+  formatFilterControlSwitch () {
+    return 'Ocultar/Mostrar controles'
+  },
+  formatFilterControlSwitchHide () {
+    return 'Ocultar controles'
+  },
+  formatFilterControlSwitchShow () {
+    return 'Mostrar controles'
+  }
+}
 
-})(jQuery);
+$.extend($.fn.bootstrapTable.defaults, $.fn.bootstrapTable.locales['es-AR'])
