6edad4b375f9deff2017b4ff53f8dd475b7aeae3
Prasanth Mohan <prasanthm@sangoma.com>
2026-07-08 09:56:05 +0000

    FREEI-2959 user reset and edit template issue resolved

--- htdocs/includes/User.class.php
+++ htdocs/includes/User.class.php
@@ -49,7 +49,7 @@ class User {
 				break;
 			case 'saveTemplate':
 			case 'resetTemplate':
-				return $this->uid;
+				return (bool) $this->getUser();
 			break;
 			default:
 				return false;
@@ -153,13 +153,15 @@ class User {
 			break;
 			case 'saveTemplate':
 				$tempId = $_REQUEST['tempid'];
-				$uId = $this->uid;
+				$user = $this->getUser();
+				$uId = !empty($user['id']) ? $user['id'] : null;
 				if(!empty($tempId) && !empty($uId)) {
 					return $this->saveTemplateById($tempId, $uId);
 				}
 			break;
 			case 'resetTemplate':
-				$uId = $this->uid;
+				$user = $this->getUser();
+				$uId = !empty($user['id']) ? $user['id'] : null;
 				if(!empty($uId)) {
 					return $this->resetTemplateById($uId);
 				}
