c8a6ec9f1eb8cc2bbe861124045a999527d0730a
Philip Joseph <jphilip@sangoma.com>
2026-07-09 08:01:08 -0400

    Auth issue

--- node/lib/auth.js
+++ node/lib/auth.js
@@ -8,31 +8,30 @@ module.exports.init = function(fpbx) {
 };
 
 module.exports.checkAuth = function(socket, next) {
-	var auth = false,
-			address = null,
-			suppliedToken = (typeof socket.handshake.query.token != "undefined") ? socket.handshake.query.token : "empty";
+	var suppliedToken = (typeof socket.handshake.query.token != "undefined") ? socket.handshake.query.token : "empty",
+			address = socket.handshake.address;
 
-	suppliedToken = freepbx.db.escape(suppliedToken);
-	address = freepbx.db.escape(socket.handshake.address);
 	address = address.replace(/^::ffff:([\d]+\.)/, "$1"); //ipv4 mapped into ipv6
-	var prep = freepbx.db.prepare('SELECT * FROM ucp_sessions WHERE session = :session AND address = :address');
-	freepbx.db.queryStream(prep({ session: suppliedToken, address: address }))
-		.on('data', function (row) {
-			socket.ucpUid = row.uid;
-			var prep = freepbx.db.prepare('UPDATE ucp_sessions SET socketid = :socketid WHERE session = :session AND address = :address');
-			freepbx.db.queryStream(prep({ session: suppliedToken, address: address, socketid: socket.id }));
-			auth = true;
-		})
-		.on('end', function () {
-		if (auth) {
-			console.log("Token [" + suppliedToken + "] from: " + address + " was accepted");
-			next();
-		} else {
-			console.log("Token [" + suppliedToken + "] from: " + address + " was rejected");
-			next(new Error("not authorized"));
+
+	freepbx.db.query(
+		'SELECT * FROM ucp_sessions WHERE session = ? AND address = ?',
+		[suppliedToken, address]
+	).then(function(rows) {
+		if (rows && rows.length) {
+			socket.ucpUid = rows[0].uid;
+			return freepbx.db.query(
+				'UPDATE ucp_sessions SET socketid = ? WHERE session = ? AND address = ?',
+				[socket.id, suppliedToken, address]
+			).then(function() {
+				console.log("Token [" + suppliedToken + "] from: " + address + " was accepted");
+				next();
+			});
 		}
-	}).on("error", function(e) {
-		console.log("Error while checking authorization?");
+		console.log("Token [" + suppliedToken + "] from: " + address + " was rejected");
+		next(new Error("not authorized"));
+	}).catch(function(e) {
+		console.log("Error while checking authorization?", e);
 		next(new Error("not authorized"));
 	});
 };
+
